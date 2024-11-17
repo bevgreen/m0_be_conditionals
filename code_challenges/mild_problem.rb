31b34c77619d251087f6cc847b282d32fad42361
Beverly Green <greenbev000@gmail.com>
2024-11-17 16:50:19 -0600

    Answered mild question

---
+++ code_challenges/mild_problem.rb
@@ -0,0 +1,9 @@
+## Mild
+
+#Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+num1 = 103
+if num1.even?
+    puts "even"
+else
+    puts "odd"
+end
